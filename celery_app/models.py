c43e095822285a3a44145f456263e928236be07c
Ahmet Utku Levent <ahmettlevent@gmail.com>
2022-08-03 14:57:47 +0300

    celery deleted from project

--- celery_app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-
-class LabeledData(Base):
-    __tablename__ = "labeled_data"
-    id = Column(Integer, primary_key=True,  index=True)
-    text = Column(String(255), index=True)
-    label = Column(String(255), index=True)
