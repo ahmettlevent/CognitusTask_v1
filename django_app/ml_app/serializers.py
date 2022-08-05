5ea09926701f965fc26c04c93c344c3942e652e8
Ahmet Utku Levent <ahmettlevent@gmail.com>
2022-08-05 09:07:50 +0300

    Dramatiq Doesn't Support Python <= 3.6

--- django_app/ml_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import LabeledData
-
-class LabeledDataSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = LabeledData
-        fields = ['text', 'label']
